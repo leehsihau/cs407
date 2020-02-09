936e0880e3def8c9665c53ad7aee9a61dae9a93f
leehsihau <34845784+leehsihau@users.noreply.github.com>
2020-02-09 15:41:45 -0500

    Upload

--- crawlerTest/crawler.py
+++ crawlerTest/crawler.py
@@ -1,19 +1,19 @@
 import datetime
+import time
 from time import sleep
 
-import requests
+import firebase as firebase
+import schedule as schedule
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 import urllib.request
-import xml.etree.ElementTree as ET
 import json
 
-browser = webdriver.Chrome()
+from firebase import firebase
 
 
-def webCrawl(foodId):
+
+def webCrawl(foodId, browser):
     # browser.get('https://dining.purdue.edu/menus/item/6c883ba0-e283-4086-ab01-e181a6615435')
     url = 'https://dining.purdue.edu/menus/item/'+foodId
     browser.get(url)
@@ -25,11 +25,11 @@ def webCrawl(foodId):
         print("calories: "+'0')
         return 0
     else:
-        calories = str(nutr_feature).index('calories-quantity\">')+19
-        print("calories: "+nutr_feature.span.text)
+        calories = int(nutr_feature.span.text)
+        print("calories: "+str(calories))
         return calories
 
-def allergensOfItem(item):
+'''def allergensOfItem(item):
     allergens = item['Allergens']
     retAllergens = []
     for i in range(len(allergens)):
@@ -41,34 +41,54 @@ def allergensOfItem(item):
         if isAllergy:
             retAllergens.append(allergenName)
     print("all allergies: "+str(retAllergens))
-    return retAllergens
+    return retAllergens'''
+
+def getWholeMenus():
+    browser = webdriver.Chrome()
+    courtNames = {'Ford', 'Wiley', 'Hillenbrand', 'Windsor','Earhart'}
+    firebase_ = firebase.FirebaseApplication('https://lunchmate-9b46b.firebaseio.com/', None)
+    result = firebase_.delete('/wholeMenus', None)
+    print('delete: '+str(result))
+    for courtName in courtNames:
+        time = datetime.datetime.now()
+        ftime = time.strftime("%m-%d-%Y")
+        #url = "http://api.hfs.purdue.edu/menus/v2/locations/Ford/" + str(ftime)
+        url = "http://api.hfs.purdue.edu/menus/v2/locations/"+courtName+'/'+str(ftime)
+        print("\n\n\n"+url)
+        page = urllib.request.urlopen(url)
+        data = json.loads(page.read())
+        formatted_json = json.dumps(data, indent=4)
+        # print(formatted_json)
+        meals = data['Meals']
+        # print(len(meals))
+        for i in range(len(meals)):
+            meal = meals[i]
+            print("whole meal: "+str(meal))
+            print("meal name: "+str(meal['Name']))
+            if(str(meal['Status']) == 'Closed'):
+                continue
+            print(("start time: "+str(meal['Hours']['StartTime'])))
+            stations = meal['Stations']
+            for j in range(len(stations)):
+                station = stations[j]
+                print("station: "+station['Name'])
+                items = station['Items']
+                for k in range(len(items)):
+                    print("food name: "+str(items[k]['Name']))
+                    print("food ID: "+str(items[k]['ID']))
+                    foodId = str(items[k]['ID'])
+                    calories = webCrawl(foodId, browser)
+                    #if calories is not 0:
+                        #allergens = allergensOfItem(items[k])
+                    items[k].update([('calories',calories)])
+        dbName = '/wholeMenus/'+courtName
+        result = firebase_.post(dbName, data)
+    browser.quit()
 
 def main():
-    time = datetime.datetime.now()
-    ftime = time.strftime("%m-%d-%Y")
-    url = "http://api.hfs.purdue.edu/menus/v2/locations/Ford/" + str(ftime)
-    page = urllib.request.urlopen(url)
-    data = json.loads(page.read())
-    formatted_json = json.dumps(data, indent=4)
-    #print(formatted_json)
-    meals = data['Meals']
-    #print(len(meals))
-    for i in range(len(meals)):
-        meal = meals[i]
-        print("meal name: "+str(meal['Name']))
-        print(("start time: "+str(meal['Hours']['StartTime'])))
-        stations = meal['Stations']
-        for j in range(len(stations)):
-            station = stations[j]
-            print("station: "+station['Name'])
-            items = station['Items']
-            for k in range(len(items)):
-                print("food name: "+str(items[k]['Name']))
-                print("food ID: "+str(items[k]['ID']))
-                foodId = str(items[k]['ID'])
-                calories = webCrawl(foodId)
-                if calories is not 0:
-                    allergens = allergensOfItem(items[k])
-    browser.quit()
+    schedule.every().day.at("00:02").do(getWholeMenus)
+    while 1:
+        schedule.run_pending()
+        time.sleep(1)
 
 main()
