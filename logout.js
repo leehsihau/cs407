7e26d3033c331ed95b5c6d99e8d341e7ddbbcdf3
Daniel Larken <dlarken@purdue.edu>
2020-02-23 19:51:13 -0500

    Delete files

--- logout.js
+++
@@ -1,9 +0,0 @@
-let logout = document.querySelectorAll("#logout");
-logout = addEventListener('click', e => {
-    e.preventDefault();
-    firebase.auth().signOut().then(function() {
-        // Sign-out successful.
-      }).catch(function(error) {
-        // An error happened.
-      });
-    });
