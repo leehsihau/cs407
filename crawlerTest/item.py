aa8276971905c9edcf5781ce3c83ed381e7b2db9
Peter Reckamp <preckam@purdue.edu>
2020-02-24 16:07:04 -0500

    removed extra files

--- crawlerTest/item.py
+++
@@ -1,7 +0,0 @@
-class Item:
-    def __init__(self, foodId, foodName, isVegetar, allergyList, calories):
-        self.foodId = foodId
-        self.foodName = foodName
-        self.isVegetar = isVegetar
-        self.allergyList = allergyList
-        self.calories = calories
--- crawlerTest/menu.py
+++
@@ -1,8 +0,0 @@
-class DiningCourt:
-    def __init__(self, isAvailable, sTime, eTime, stations):
-        self.isAvailable=isAvailable
-        self.sTime = sTime
-        self.eTime = eTime
-        self.stations = stations
-
-
