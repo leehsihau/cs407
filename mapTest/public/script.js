1b81c9feecb8bcdc005b83b7cdd6d57276e50f81
leehsihau <34845784+leehsihau@users.noreply.github.com>
2020-02-28 10:30:54 -0500

    Changed zoom index

--- mapTest/public/script.js
+++ mapTest/public/script.js
@@ -142,8 +142,8 @@ function saveMessagingDeviceToken() {
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map-canvas'), {
-    center: { lat: 40.454770, lng: -86.915700 },
-    zoom: 6,
+    center: { lat: 40.426764, lng: -86.919632 },
+    zoom: 15,
     mapTypeId: google.maps.MapTypeId.ROADMAP
 
   });
