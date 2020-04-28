31977771379fc03328d9ce8f4868db56e125d614
Peter Reckamp <preckam@purdue.edu>
2020-04-28 18:52:17 -0400

    started on filter

--- mapTest/public/scripts/foodmenu.js
+++ mapTest/public/scripts/foodmenu.js
@@ -1,19 +1,65 @@
+/*function getMealTime(diningCourt){
+    var time = new Date();
+    time = time.getHours();
+    var openTime; //Find breakfast open time
+    var closeTime; //Find breakfast close time
+    for(let i = 0; i < 4; i++){
+        if(time > openTime && time < closeTime){
+        return currMeal //TODO
+        }
+        openTime = //Set to open time of next meal
+        closeTime = //Set to close time of next meal
+    }
+    return "Breakfast"
+}
+*/
+var allCalories = [];
+var allCounter = 0;
+var meanCalories = 0;
 
+function getMeanCalories(){
+    let sum = 0;
+    for(let i = 0; i < allCalories.length; i++){
+        sum = sum + allCalories[i];
+    }
+    meanCalories = sum/allCalories.length;
+}
 
 function loadMenu(){
     var menus = ["menu Earhart", "menu Ford", "menu Hillenbrand", "menu Wiley", "menu Windsor"];
+    var mealtime;
     for(let j = 0; j < 5; j++){
     for(let i = 0; i < 10; i++){
-        var node = document.createElement("div");
-        node.setAttribute('class', 'menu-item');
+        var node = document.createElement("div"); //Wrapper
+        node.setAttribute('class', 'menu-item-wrapper');  
+        var node3 = document.createElement("div");
+        node3.setAttribute('class', 'menu-item'); //Item name
         var textnode = document.createTextNode("item " + i);
-        node.appendChild(textnode);
+        node3.appendChild(textnode);
         var node2 = document.createElement("div");
-        node2.setAttribute('class', 'calories');
-        var textnode2 = document.createTextNode(//TODO)
+        node2.setAttribute('class', 'calories'); //Calories
+        var calories; //Fetch calories from JSON
+        var textnode2 = document.createTextNode("Calories: ");
         node2.appendChild(textnode2);
+        node.appendChild(node3);
+        node.appendChild(node2);
         document.getElementById(menus[j]).appendChild(node);
-        document.getElementsByTagName(menus[j]).appendChild(node2);
+        allCalories.push(calories);
+        allCounter++;
     }
 }
+    getMeanCalories();
+}
+
+function filterMenu(highLow){
+    if(lowHigh == 0){ //Nothing is selected
+        var node = document.getElementsByClassName('menu-item-wrapper');
+        node.style.display = "block";
+    }
+    else if(lowHigh == 1){//If low is selected
+        var node = 
+    }
+    else{//High is selected
+
+    }
 }
