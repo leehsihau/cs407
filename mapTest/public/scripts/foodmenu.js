4f8879bab95109e2f5730c193636ca36301029ec
Peter Reckamp <preckam@purdue.edu>
2020-04-28 17:40:11 -0400

    Fixed js, moved foodmenu to map

--- mapTest/public/scripts/foodmenu.js
+++ mapTest/public/scripts/foodmenu.js
@@ -1,16 +1,19 @@
 
 
 function loadMenu(){
-    //Figure out how to grab data here
-    
-    
-    
+    var menus = ["menu Earhart", "menu Ford", "menu Hillenbrand", "menu Wiley", "menu Windsor"];
+    for(let j = 0; j < 5; j++){
     for(let i = 0; i < 10; i++){
         var node = document.createElement("div");
         node.setAttribute('class', 'menu-item');
         var textnode = document.createTextNode("item " + i);
         node.appendChild(textnode);
-        document.getElementById("menu-page-wrapper").appendChild(node);
-
+        var node2 = document.createElement("div");
+        node2.setAttribute('class', 'calories');
+        var textnode2 = document.createTextNode(//TODO)
+        node2.appendChild(textnode2);
+        document.getElementById(menus[j]).appendChild(node);
+        document.getElementsByTagName(menus[j]).appendChild(node2);
     }
+}
 }
